04f832a65cadb62ca38f85fa5930907aa073353a
mostafaomar12 <mostafaomar1215@gmail.com>
2024-05-01 06:08:47 +0300

    update server url

--- src/utlits/Variables.js
+++ src/utlits/Variables.js
@@ -1,2 +1,3 @@
 // export const server = 'http://127.0.0.1:8000/'
-export const server = 'http://193.160.119.244:8008/'
+// export const server = 'http://193.160.119.244:8008/'
+export const server = 'https://193.160.119.244/'
