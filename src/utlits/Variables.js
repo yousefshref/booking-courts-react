cba8855cb8003bc00db9838a88dae8ad6f7ecb27
yousefshref <yb200a5@gmail.com>
2024-05-10 03:08:11 +0200

    notifications v2

--- src/utlits/Variables.js
+++ src/utlits/Variables.js
@@ -1,3 +1,3 @@
-export const server = "http://127.0.0.1:8000/";
+// export const server = "http://127.0.0.1:8000/";
 // export const server = 'http://193.160.119.244:8008/'
-// export const server = 'https://sports.api.linkawyx.com:8005/'
+export const server = "https://sports.api.linkawyx.com:8005/";
